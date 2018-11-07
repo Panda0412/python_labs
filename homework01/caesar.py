297bc5c79655dfc6178aacc770ef706f88b8f2db
Panda0412 <nasb_99@mail.ru>
2018-11-07 15:31:55 +0300

    Реализована функция decrypt_caesar()

--- homework01/caesar.py
+++ homework01/caesar.py
@@ -15,7 +15,7 @@ def encrypt_caesar(plaintext):
     slovo=[i for i in plaintext]
     ciphertext=''
     for i in range(len(slovo)):
-        simbol=ord(word[i])
+        simbol=ord(slovo[i])
         if 65 <= simbol <= 87 or 97 <= simbol <= 119:
             slovo[i]=chr(simbol+3)
         elif 88 <= simbol <= 90 or 120 <= simbol <= 122:
@@ -38,4 +38,13 @@ def decrypt_caesar(ciphertext):
     ''
     """
     # PUT YOUR CODE HERE
+    slovo=[i for i in ciphertext]
+    plaintext=''
+    for i in range(len(slovo)):
+        simbol=ord(slovo[i])
+        if 68 <= simbol <= 90 or 100 <= simbol <= 122:
+            slovo[i]=chr(simbol-3)
+        elif 65 <= simbol <= 67 or 97 <= simbol <= 99:
+            slovo[i]=chr(simbol+23)
+        plaintext+=slovo[i]
     return plaintext
