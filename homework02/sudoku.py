1dda7c852077f506313453bbd61636a22700aa2b
Panda0412 <nasb_99@mail.ru>
2018-11-18 16:15:20 +0300

    Реализована функция solve()

--- homework02/sudoku.py
+++ homework02/sudoku.py
@@ -182,7 +182,18 @@ def solve(grid):
     >>> solve(grid)
     [['5', '3', '4', '6', '7', '8', '9', '1', '2'], ['6', '7', '2', '1', '9', '5', '3', '4', '8'], ['1', '9', '8', '3', '4', '2', '5', '6', '7'], ['8', '5', '9', '7', '6', '1', '4', '2', '3'], ['4', '2', '6', '8', '5', '3', '7', '9', '1'], ['7', '1', '3', '9', '2', '4', '8', '5', '6'], ['9', '6', '1', '5', '3', '7', '2', '8', '4'], ['2', '8', '7', '4', '1', '9', '6', '3', '5'], ['3', '4', '5', '2', '8', '6', '1', '7', '9']]
     """
-
+    pos=find_empty_positions(grid)
+    if pos[0]==-1:
+        return grid
+    i,j=pos
+    pos_val=find_possible_values(grid, pos)
+    for a in pos_val:
+        grid[i][j]=a
+        solution=solve(grid)
+        if solution:
+            return solution
+    grid[i][j]='.'
+    return None
 
 
 def check_solution(solution):
