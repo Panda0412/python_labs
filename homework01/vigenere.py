f84f8694532215ec2ccbf9385c013ab227d39eff
Panda0412 <nasb_99@mail.ru>
2018-12-03 20:31:16 +0300

    Добавлено соответствие РЕР8

--- homework01/vigenere.py
+++ homework01/vigenere.py
@@ -9,22 +9,21 @@ def encrypt_vigenere(plaintext, keyword):
     >>> encrypt_vigenere("ATTACKATDAWN", "LEMON")
     'LXFOPVEFRNHR'
     """
-    # PUT YOUR CODE HERE
-    s=[i for i in plaintext]
-    key=[i for i in keyword*(len(plaintext)//len(keyword)+1)]
-    ciphertext=''
+    s = [i for i in plaintext]
+    key = [i for i in keyword*(len(plaintext)//len(keyword)+1)]
+    ciphertext = ''
     for i in range(len(s)):
-        simbol=ord(s[i])
-        simkey=ord(key[i])
+        simbol = ord(s[i])
+        simkey = ord(key[i])
         if 65 <= simkey <= 90:
-            simkey-=65
+            simkey -= 65
         elif 97 <= simkey <= 122:
-            simkey-=97
+            simkey -= 97
         if 65 <= simbol <= 90-simkey or 97 <= simbol <= 122-simkey:
-            s[i]=chr(simbol+simkey)
+            s[i] = chr(simbol+simkey)
         elif 90-simkey < simbol <= 90 or 122-simkey < simbol <= 122:
-            s[i]=chr(simbol+simkey-26)
-        ciphertext+=s[i]
+            s[i] = chr(simbol+simkey-26)
+        ciphertext += s[i]
     return ciphertext
 
 
@@ -39,20 +38,19 @@ def decrypt_vigenere(ciphertext, keyword):
     >>> decrypt_vigenere("LXFOPVEFRNHR", "LEMON")
     'ATTACKATDAWN'
     """
-    # PUT YOUR CODE HERE
-    s=[i for i in ciphertext]
-    key=[i for i in keyword*(len(ciphertext)//len(keyword)+1)]
-    plaintext=''
+    s = [i for i in ciphertext]
+    key = [i for i in keyword*(len(ciphertext)//len(keyword)+1)]
+    plaintext = ''
     for i in range(len(s)):
-        simbol=ord(s[i])
-        simkey=ord(key[i])
+        simbol = ord(s[i])
+        simkey = ord(key[i])
         if 65 <= simkey <= 90:
-            simkey-=65
+            simkey -= 65
         elif 97 <= simkey <= 122:
-            simkey-=97
+            simkey -= 97
         if 65+simkey <= simbol <= 90 or 97+simkey <= simbol <= 122:
-            s[i]=chr(simbol-simkey)
+            s[i] = chr(simbol-simkey)
         elif 65 <= simbol < 65+simkey or 97 <= simbol < 97+simkey:
-            s[i]=chr(simbol-simkey+26)
-        plaintext+=s[i]
+            s[i] = chr(simbol-simkey+26)
+        plaintext += s[i]
     return plaintext
