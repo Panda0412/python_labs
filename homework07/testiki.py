605c3ae68d1ff997ae25eb37fa33d6920e4a55c3
Panda0412 <nasb_99@mail.ru>
2019-03-23 19:48:33 +0300

    Добавлены новые тесты

--- homework07/testiki.py
+++ homework07/testiki.py
@@ -11,6 +11,13 @@ class PoolTests(unittest.TestCase):
         self.assertEqual(10, pool.max_workers)
         self.assertEqual(3, pool.mem_usage)
 
+    def test_change_mem(self):
+        gb = ProcessPool(min_workers=2, max_workers=10, mem_usage='5Gb').mem_usage
+        mb = ProcessPool(min_workers=2, max_workers=10, mem_usage='3075mB').mem_usage
+        kb = ProcessPool(min_workers=2, max_workers=10, mem_usage='100000kb').mem_usage
+        b = ProcessPool(min_workers=2, max_workers=10, mem_usage='8500000B').mem_usage
+        self.assertEqual([gb, mb, kb, b], [5.0, 3.003, 0.095, 0.008])
+
     def test_mem_err_one(self):
         pool = ProcessPool(min_workers=100, max_workers=110, mem_usage='1gb')
         q = mp.Queue()
@@ -24,7 +31,7 @@ class PoolTests(unittest.TestCase):
             raise Warning
 
     def test_mem_err_two(self):
-        pool = ProcessPool(min_workers=2, max_workers=10, mem_usage='0gb')
+        pool = ProcessPool(min_workers=2, max_workers=10, mem_usage='5000kb')
         q = mp.Queue()
         q.put(8)
         q.put(13)
@@ -54,6 +61,14 @@ class PoolTests(unittest.TestCase):
         q.put(10)
         pool.map(heavy_computation, q)
         self.assertEqual(pool.amt_workers, 1)
+
+    def test_correct_mem(self):
+        pool = ProcessPool(min_workers=2, max_workers=10, mem_usage='512mb')
+        q = mp.Queue()
+        q.put(4)
+        q.put(12)
+        amt_workers, p_mem_usage = pool.map(heavy_computation, q)
+        self.assertLessEqual(amt_workers * p_mem_usage, 0.5)
 
 
 if __name__ == '__main__':
